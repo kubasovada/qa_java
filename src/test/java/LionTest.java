e097f7f012674a75613e1fea3b5a78f43e0bac41
dkubasova <321Ewq321>
2022-05-16 15:17:43 +0300

    added catchException in LionTest

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -9,6 +9,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest {
@@ -39,8 +40,21 @@ public class LionTest {
     boolean actual = lion.doesHaveMane();
     boolean expected = true;
     assertEquals(expected, actual);
-
   }
 
+  @Test//(expected=Exception.class)
+  public void catchException() throws Exception {
+
+    Exception exception = null;
+    try {
+      Lion lion = new Lion(feline, "Самецrf");
+    } catch (Exception ex) {
+      exception = ex;
+    }
+    String expectedTextException = "Используйте допустимые значения пола животного - самей или самка";
+    String actual = exception.getMessage();
+    assertNotNull(exception);
+    assertEquals(expectedTextException, actual);
+  }
 
 }
