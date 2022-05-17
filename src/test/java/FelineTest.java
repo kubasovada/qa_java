43fc682c31cc0b10e0608580f1b235cf556c4dd3
dkubasova <321Ewq321>
2022-05-17 18:29:05 +0300

    finalized FELINE tests

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,22 +1,25 @@
 import com.example.Feline;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.List;
 import static org.junit.Assert.assertEquals;
 
-//@RunWith(MockitoJUnitRunner.class)
-@RunWith(Parameterized.class)
+@RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
-//  @Mock
-//  Animal animal;
 
-  private final int kittensCount;
+  @Spy
+  Feline feline;
 
-  public FelineTest(int kittensCount) {
-    this.kittensCount = kittensCount;
+  @Test
+  public void eatMeatReturnsCorrectValue() throws Exception {
+    Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+    List<String> expected = List.of("Животные", "Птицы", "Рыба");
+    List<String> actual = feline.eatMeat();
+    assertEquals(expected, actual);
   }
-
 
   @Test
   public void getFamilyReturnsCorrectValue() {
@@ -25,17 +28,6 @@ public class FelineTest {
     String actual = feline.getFamily();
     assertEquals(expected, actual);
   }
-
-
-  @Parameterized.Parameters
-  public static Object[][] getKittensCount() {
-    return new Object[][] {
-            { 1},
-            { 2},
-            { 3},
-            {10}
-    };
-  }
   @Test
   public void getKittensReturnCorrectValue() {
     Feline feline = new Feline();
@@ -43,24 +35,5 @@ public class FelineTest {
     int actual = feline.getKittens();
     assertEquals(expected, actual);
   }
-
-  @Test
-  public void getKittensWithCountReturnCorrectValue() {
-    Feline feline = new Feline();
-    //int kittensCount = 0;
-    int expected = kittensCount;
-    int actual = feline.getKittens(kittensCount);
-    assertEquals(expected, actual);
-  }
-
-//  @Test
-//  public void eatMeatReturnsCorrectValue() throws Exception {
-//    Feline feline = new Feline();
-//    Mockito.when(animal.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
-//    List<String> expected = List.of("Животные", "Птицы", "Рыба");
-//    List<String> actual = feline.eatMeat();
-//    assertEquals(expected, actual);
-//  }
-
 
 }
--- src/test/java/FelineWithParametersTest.java
+++ src/test/java/FelineWithParametersTest.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class FelineWithParametersTest {
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class FelineWithParametersTest {
+
+  private final int kittensCount;
+
+  public FelineWithParametersTest(int kittensCount) {
+    this.kittensCount = kittensCount;
+  }
+
+  @Parameterized.Parameters
+  public static Object[][] getKittensCount() {
+    return new Object[][] {
+            { 1},
+            { 2},
+            { 3},
+            {10},
+            {15}
+    };
+  }
+
+  @Test
+  public void getKittensWithCountReturnCorrectValue() {
+    Feline feline = new Feline();
+    //int kittensCount = 0;
+    int expected = kittensCount;
+    int actual = feline.getKittens(kittensCount);
+    assertEquals(expected, actual);
+  }
 }
